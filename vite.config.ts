fd7a872ffdd64857136e30910d8db789007cbe09
Nikita <abooshsmartkid@gmail.com>
2026-07-04 04:33:32 +0300

    Add website accessibility support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        accessibility: fileURLToPath(
+          new URL('./accessibility/index.html', import.meta.url),
+        ),
+      },
+    },
+  },
 })
